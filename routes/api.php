16741332cea3a570886e5a12d82a860b78b72138
Ika-Taro <black.jkh@gmail.com>
2022-04-29 14:01:22 +0900

    laravel側でindex()でblog一覧の取得を実装

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['middleware' => 'api'], function(){
+  Route::get('blogs', 'App\Http\Controllers\Api\BlogController@index');
 });
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
