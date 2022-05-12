5b6121de848b9ca646d00e98783ccb2ab65467ee
Ika-Taro <black.jkh@gmail.com>
2022-05-12 21:59:34 +0900

    Update Pages.tsx

--- resources/scripts/Pages.tsx
+++ resources/scripts/Pages.tsx
@@ -143,20 +143,36 @@ export function Blog() {
   )
   const totalStars = 3;
 
+  let PostButton: any = "";
+
+    if (!localStorage.getItem('auth_token')){
+        PostButton = (
+          <>
+          </>
+        );
+      } else {
+        PostButton = (
+        <div className="relative">
+          <div className="fixed bottom-5 right-5">
+            <button type="button" className="inline-block rounded-full bg-blue-600 text-white leading-normal uppercase shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out w-9 h-9 " data-bs-toggle="modal" data-bs-target="#exampleModalCenteredScrollable">
+              <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="download"
+                className="w-3 mx-auto" role="img" xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 512 512">
+                <path fill="currentColor"
+                  d="M216 0h80c13.3 0 24 10.7 24 24v168h87.7c17.8 0 26.7 21.5 14.1 34.1L269.7 378.3c-7.5 7.5-19.8 7.5-27.3 0L90.1 226.1c-12.6-12.6-3.7-34.1 14.1-34.1H192V24c0-13.3 10.7-24 24-24zm296 376v112c0 13.3-10.7 24-24 24H24c-13.3 0-24-10.7-24-24V376c0-13.3 10.7-24 24-24h146.7l49 49c20.1 20.1 52.5 20.1 72.6 0l49-49H488c13.3 0 24 10.7 24 24zm-124 88c0-11-9-20-20-20s-20 9-20 20 9 20 20 20 20-9 20-20zm64 0c0-11-9-20-20-20s-20 9-20 20 9 20 20 20 20-9 20-20z">
+                </path>
+              </svg>
+            </button>
+          </div>
+        </div>
+
+        );
+    }
+
   return (
     <>
-      <div className="relative">
-        <div className="fixed bottom-5 right-5">
-          <button type="button" className="inline-block rounded-full bg-blue-600 text-white leading-normal uppercase shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out w-9 h-9 " data-bs-toggle="modal" data-bs-target="#exampleModalCenteredScrollable">
-            <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="download"
-              className="w-3 mx-auto" role="img" xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 512 512">
-              <path fill="currentColor"
-                d="M216 0h80c13.3 0 24 10.7 24 24v168h87.7c17.8 0 26.7 21.5 14.1 34.1L269.7 378.3c-7.5 7.5-19.8 7.5-27.3 0L90.1 226.1c-12.6-12.6-3.7-34.1 14.1-34.1H192V24c0-13.3 10.7-24 24-24zm296 376v112c0 13.3-10.7 24-24 24H24c-13.3 0-24-10.7-24-24V376c0-13.3 10.7-24 24-24h146.7l49 49c20.1 20.1 52.5 20.1 72.6 0l49-49H488c13.3 0 24 10.7 24 24zm-124 88c0-11-9-20-20-20s-20 9-20 20 9 20 20 20 20-9 20-20zm64 0c0-11-9-20-20-20s-20 9-20 20 9 20 20 20 20-9 20-20z">
-              </path>
-            </svg>
-          </button>
-        </div>
+      <div>
+        {PostButton}
       </div>
 
       <div className="modal fade fixed top-0 left-0 hidden w-full h-full outline-none overflow-x-hidden overflow-y-auto" id="exampleModalCenteredScrollable" aria-labelledby="exampleModalCenteredScrollable" aria-modal="true" role="dialog">
